b405f9fd529b21579fd494364d95017f00ecaa85
franciscocedillos <franciscocedillos@outlook.com>
2016-10-30 23:48:40 -0600

    Index
    :v

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Index</title>
+</head>
+<body>
+		
+	
+	<h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam accusamus, repudiandae explicabo quae qui consequuntur ex facilis, odio eveniet voluptatibus dolorum ipsa sequi voluptate, nobis, distinctio laboriosam? Veritatis, cumque, cum?</h1>
+
+
+</body>
+</html>
