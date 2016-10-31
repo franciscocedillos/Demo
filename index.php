66c3d5c261bb70c786a2fb3eb4578b5544650294
franciscocedillos10 <franciscocedillos10@gmail.com>
2016-10-30 23:51:09 -0600

    Index corregido
    Ahora con título y texto

--- index.php
+++ index.php
@@ -7,7 +7,9 @@
 <body>
 		
 	
-	<h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam accusamus, repudiandae explicabo quae qui consequuntur ex facilis, odio eveniet voluptatibus dolorum ipsa sequi voluptate, nobis, distinctio laboriosam? Veritatis, cumque, cum?</h1>
+	<center><h1>Lorem</h1></center>
+	<br><br>
+	Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam accusamus, repudiandae explicabo quae qui consequuntur ex facilis, odio eveniet voluptatibus dolorum ipsa sequi voluptate, nobis, distinctio laboriosam? Veritatis, cumque, cum?
 
 
 </body>
